a4254ebda598341527a1e6fcf488486386d2f677
brunagaspar <61790436+brunagaspar@users.noreply.github.com>
2020-04-20 15:47:36 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# Ponha aqui os dados do seu robozinho
+bottoken='NzEzZDk2NTAtODcxNy00YzIwLTk5NGItYTNhYTYzNzczZjk2ZjY3NDk3ODQtYTUy_PF84_a892a6ed-b823-46b8-8336-847d8f4722ae'
+#exemplo='Yia0dsfdsfsdf...' - tirado do site developer.webex.com
+botmail="waterinformation@webex.bot"
+#exemplo='mybotmail@spark.io' - tirado do site developer.webex.com
+
+# Ponha aqui os dados do seu Webhook
+# webhook_url o endereco publico onde esta o app
+webhook_url="https://botwebex.herokuapp.com/"
+# exemplo webhook_url="https://mywebhook.com"
+# webhook_name e' o nome do gatilho que o Webex Teams gera para seu aplicativo entender
+webhook_name="bot 2 - water information"
+# exemplo webhook_name="meuprimeirochatbot"

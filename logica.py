ba3d66b9fb736f8430ae63d7614ace37f9ff253d
brunagaspar <61790436+brunagaspar@users.noreply.github.com>
2020-04-24 10:32:00 -0300

    Update logica.py

--- logica.py
+++ logica.py
@@ -40,6 +40,8 @@ def logica(comando,usermail):
         msg="Olá, Humano! Antes de liberar o escoamento da água utilizada nos processos indústriais, verifique comigo se o tanque especificado já está pronto para voltar ao meio ambiente. De qual tanque você gostaria de consultar a temperatura?\n"
         msg=msg+"Digite 1 para ver o Tanque A.\n"
 
+if box == "1"
+	msg="temperatura do tanque A xx graus. 
 
 
     if len(sp)>2:
